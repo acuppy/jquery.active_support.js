c94bd6a26cb2e01e49d63439602c217430c5710d
acuppy <acuppy@gmail.com>
2013-06-03 15:47:20 -0700

    updated the spec and remove old assertions

--- spec/spec/jquery.active_support.spec.js
+++ spec/spec/jquery.active_support.spec.js
@@ -1,40 +1,33 @@
-
 describe("$.isBlank", function() {
-  it("binds to $", function() {
-    expect($.isBlank).toBeDefined();
+  it("arrays", function() {
+    expect($.isBlank([])).toBeTruthy();
+    expect($.isBlank(['foo', 'bar'])).toBeFalsy();
   });
 
-  describe(".isBlank", function() {
-    it("arrays", function() {
-      expect($.isBlank([])).toBeTruthy();
-      expect($.isBlank(['foo', 'bar'])).toBeFalsy();
-    });
-
-    it("strings", function() {
-      expect($.isBlank('')).toBeTruthy();
-      expect($.isBlank('foo bar')).toBeFalsy();
-    });
-
-    it("objects", function() {
-      expect($.isBlank({})).toBeTruthy();
-      expect($.isBlank({ foo: 'bar' })).toBeFalsy();
-    });
+  it("strings", function() {
+    expect($.isBlank('')).toBeTruthy();
+    expect($.isBlank('foo bar')).toBeFalsy();
   });
 
-  describe(".isPresent", function() {
-    it("arrays", function() {
-      expect($.isPresent([])).toBeFalsy();
-      expect($.isPresent(['foo', 'bar'])).toBeTruthy();
-    });
+  it("objects", function() {
+    expect($.isBlank({})).toBeTruthy();
+    expect($.isBlank({ foo: 'bar' })).toBeFalsy();
+  });
+});
 
-    it("strings", function() {
-      expect($.isPresent('')).toBeFalsy();
-      expect($.isPresent('foo bar')).toBeTruthy();
-    });
+describe("$.isPresent", function() {
+  it("arrays", function() {
+    expect($.isPresent([])).toBeFalsy();
+    expect($.isPresent(['foo', 'bar'])).toBeTruthy();
+  });
 
-    it("objects", function() {
-      expect($.isPresent({})).toBeFalsy();
-      expect($.isPresent({ foo: 'bar' })).toBeTruthy();
-    });
+  it("strings", function() {
+    expect($.isPresent('')).toBeFalsy();
+    expect($.isPresent('foo bar')).toBeTruthy();
+  });
+
+  it("objects", function() {
+    expect($.isPresent({})).toBeFalsy();
+    expect($.isPresent({ foo: 'bar' })).toBeTruthy();
   });
 });
