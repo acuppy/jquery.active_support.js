b3414798551c07c1e59a8cdea3c73c863a229812
acuppy <acuppy@gmail.com>
2013-06-09 23:06:59 -0700

    $.redirect

--- jquery.active_support.js
+++ jquery.active_support.js
@@ -23,11 +23,42 @@
     return !$.isBlank.apply(this, arguments);
   }
 
-  $.isLink = $.isURL = function(){
+  $.redirect = function(url, opt){
+    var opt = opt || {};
 
+    if( ! $.isURL(url) ) $.error('A valid URL expected -- received' + url.toString());
+
+    if( $.type(opt) == 'string' ) opt = { confirm: opt };
+
+    opt = $.extend({
+      confirm: false,
+      only: [],
+      exclude: [] 
+    }, opt, arguments[2] || {});
+
+    var included = $.map( opt.only, function(elm, i){
+      if( $.inArray( elm, opt.exclude ) == -1 ){
+        return elm;
+      }
+    });
+
+    if( $.isBlank(included) || $.grep( included, function(elm, i){
+      switch( $.type(elm) ){
+        case 'string': return elm == url; break;
+        case 'regexp': return url.search(elm) != -1; break;
+      }
+    }).length > 0 ){
+      if( $.type(opt.confirm) == 'string' ){
+        if( window.confirm(opt.confirm) ) $.redirect.runner(url); 
+        return;
+      }
+      $.redirect.runner(url); return;
+    };
   }
 
-  $.isPhone = $.isPhoneNumber = function(){
+  $.redirect.runner = (function(){
+    return window.location.replace;
+  })();
 
   }
 
--- spec/spec/jquery.active_support.spec.js
+++ spec/spec/jquery.active_support.spec.js
@@ -69,3 +69,92 @@ describe("$.isPresent", function() {
     });
   });
 });
+
+describe("$.redirect", function() {
+  beforeEach(function() {
+    spyOn($.redirect, 'runner').andReturn($.noop);
+  });
+
+  describe("without options", function() {
+    beforeEach(function() {
+      $.redirect('http://example.com');
+    });
+
+    it("should redirect", function() {
+      expect($.redirect.runner).toHaveBeenCalled();
+    });
+  });
+
+  describe("with options", function() {
+    describe("confirm", function() {
+      describe("when returning true", function() {
+        beforeEach(function() {
+          spyOn(window, 'confirm').andReturn(true)
+        });
+
+        afterEach(function() {
+          expect($.redirect.runner).toHaveBeenCalled();
+        });
+
+        it("should confirm from second argument as a hash", function() {
+          $.redirect('http://example.com', { confirm: "Are you sure?" })
+          expect(window.confirm).toHaveBeenCalledWith("Are you sure?");
+        });
+
+        it("should confirm from second argument as a string", function() {
+          $.redirect('http://example.com', "Are you sure?")
+          expect(window.confirm).toHaveBeenCalledWith("Are you sure?");
+        });
+      });
+
+      describe("when returning false", function() {
+        beforeEach(function() {
+          spyOn(window, 'confirm').andReturn(false)
+          $.redirect('http://example.com', "Are you sure?")
+        });
+
+        it("should confirm", function() {
+          expect(window.confirm).toHaveBeenCalledWith("Are you sure?");
+        });
+
+        it("should not redirect", function() {
+          expect($.redirect.runner).not.toHaveBeenCalled();
+        });
+      });
+
+    });
+
+    describe("whitelisting", function() {
+      beforeEach(function() {
+        spyOn(window, 'confirm').andReturn(true)
+      });
+
+      describe("only", function() {
+        describe("strings", function() {
+          it("should redirect with a matching string", function() {
+            $.redirect('http://example.com', { only: ['http://example.com'] } );
+            expect($.redirect.runner).toHaveBeenCalledWith('http://example.com');
+          });
+
+          it("should NOT redirect with a mismatched string", function() {
+            $.redirect('http://example.com', { only: ['http://example_invalid.com'] } );
+            expect($.redirect.runner).not.toHaveBeenCalledWith('http://example.com');
+          });
+        });
+
+        describe("regexp", function() {
+          it("should redirect with a matching string", function() {
+            $.redirect('http://example.com', { only: [/example\.com$/] } );
+            expect($.redirect.runner).toHaveBeenCalledWith('http://example.com');
+          });
+
+          it("should NOT redirect with a mismatched string", function() {
+            $.redirect('http://example.com', { only: [/example\_invalid\.com$/] } );
+            expect($.redirect.runner).not.toHaveBeenCalledWith('http://example.com');
+          });
+        });
+      });
+    });
+  });
+
+});
